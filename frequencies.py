2e905b0fdf8d222db161197e75a77d6435bb7c3e
Ivo Verneris <ivo.verneris@kcl.ac.uk>
2022-09-28 20:36:32 +0100

    Fixed indentation in frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -5,9 +5,9 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for item in items:
-    str(item)
-    if item in frequencies.keys():
-    	frequencies[item] += 1
-    else:
-    	frequencies[item] = 1
+        item = str(item)
+        if item in frequencies.keys():
+        	frequencies[item] += 1
+        else:
+        	frequencies[item] = 1
     return frequencies
